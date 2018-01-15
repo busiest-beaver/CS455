6a7b554ded9a81f69f733d73c33e918da5787778
Steven Smith <deans93@uw.edu>
2018-01-14 21:54:29 -0800

    Initial commit.
    + README
    + CLI argument parser
    + Python module structure
    + tcss455 python script

---
+++ ml/baseline.py
@@ -0,0 +1,9 @@
+class baseline:
+  def __init__(self):
+    '''Constructor for this class.'''
+  
+  def process(self, **kwargs):
+    input_dir = kwargs['input_dir']
+    output_dir = kwargs['output_dir']
+
+    
