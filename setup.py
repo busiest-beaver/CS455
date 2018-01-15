a88aca816467a88de82061505db45eac7e82588a
Steven Smith <nullspace000@gmail.com>
2018-01-15 11:25:20 -0800

    Package python project.
    Makes the python project an installable package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tcss455group7',
+        version='0.1',
+        description='Machine learning group project.',
+        url='https://github.com/alsohas/CS455',
+        author='Group 7',
+        author_email='deans93@uw.edu',
+        license='MIT',
+        packages=['tcss455group7'],
+        install_requires=[],
+        zip_safe=False)
--- tcss455group7/baseline.py
+++ tcss455group7/baseline.py
@@ -1,9 +1,7 @@
 class baseline:
   def __init__(self):
     '''Constructor for this class.'''
-  
+
   def process(self, **kwargs):
     input_dir = kwargs['input_dir']
     output_dir = kwargs['output_dir']
-
-    
