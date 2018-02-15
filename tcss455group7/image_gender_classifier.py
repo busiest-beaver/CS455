7f5a3fa0bdb4ada8fb41f1a7005c1d50aef9866d
Steven Smith <nullspace000@gmail.com>
2018-02-15 08:13:41 -0800

    Update image gender classifier.

--- tcss455group7/image_gender_classifier.py
+++ tcss455group7/image_gender_classifier.py
@@ -4,6 +4,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.applications.xception import Xception
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from os.path import exists, basename, splitext, join
@@ -11,52 +12,27 @@ from os.path import exists, basename, splitext, join
 class image_gender_classifier:
     # dimensions of our images.
     __IMG_WIDTH, __IMG_HEIGHT = 200, 200
-    __WEIGHTS_PATH = '/data/gender.weights'
+    __WEIGHTS_PATH = '/data/weights/gender_xception.hdf5'
     __BATCH_SIZE = 16
 
     def __init__(self):
         '''empty constructor'''
 
     def __get_model(self):
-        if K.image_data_format() == 'channels_first':
-            input_shape = (3, self.__IMG_WIDTH, self.__IMG_HEIGHT)
-        else:
-            input_shape = (self.__IMG_WIDTH, self.__IMG_HEIGHT, 3)
-
+        xcept = Xception(include_top=False, weights='imagenet', input_shape = (200,200,3))
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Conv2D(32, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Activation('relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(1))
-        model.add(Activation('sigmoid'))
-
-        model.compile(loss='binary_crossentropy',
-                      optimizer='rmsprop',
-                      metrics=['accuracy'])
-
-        if exists(self.__WEIGHTS_PATH):
-            model.load_weights(self.__WEIGHTS_PATH)
-        else:
-            print('Gender model weights not found.')
-            model = None
+        model.add(Flatten(input_shape=xcept.output_shape[1:]))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.50))
+        model.add(Dense(1, activation='sigmoid'))
+        model = Model(inputs=xcept.input, outputs=model(xcept.output))
+        model.load_weights(self.__WEIGHTS_PATH)
         return model
 
     def test(self, **kwargs):
         prediction = None
         input_dir = kwargs['input_dir']
+
 
         model = self.__get_model()
         if (model == None): return {}
