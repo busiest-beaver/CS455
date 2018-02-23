1912bf19138b217cdc07d71ae2dae43485255c3b
Wilder Garcia <wildergarcia@D-10-16-17-74.dhcp4.washington.edu>
2018-02-22 18:00:40 -0800

    .

--- likes/likesID.py
+++ likes/likesID.py
@@ -16,9 +16,18 @@ from sklearn.pipeline import make_pipeline
 import matplotlib.pyplot as plt
 from sklearn.linear_model import PassiveAggressiveRegressor
 import pickle
+import random
+
+
 
 df1 = pd.read_csv('/Users/wildergarcia/Desktop/tcss455/training/profile/profile.csv', index_col=0)
 df2 = pd.read_csv('/Users/wildergarcia/Desktop/tcss455/training/relation/relation.csv', index_col=0)
+
+# df1 = np.arange(len(df1))
+# random.shuffle(df1)
+#
+
+
 
 df2['like_id'] = df2['like_id'].astype(str)
 # merged profile and relation files into one dataframe based in user id
@@ -47,12 +56,15 @@ print(df5.columns)
 # Splitting the data into 300 training instances and 104 test instances
 n = 1500
 all_Ids = np.arange(len(df5))
+random.shuffle(all_Ids
+
+
+
+)
 test_Ids = all_Ids[0:n]
 train_Ids = all_Ids[n:]
 data_test =df5.loc[test_Ids, :]
 data_train = df5.loc[train_Ids, :]
-
-
 
 # Training a Naive Bayes model
 count_vect = CountVectorizer() # this mean a transformation in the training data
@@ -69,10 +81,8 @@ y_predicted = clf.predict(X_test)
 
 # Reporting on classification performance
 print("Accuracy: %.2f" % accuracy_score(y_test,y_predicted))
-# classes = ['Male','Female']
-# cnf_matrix = confusion_matrix(y_test,y_predicted,labels=classes)
-# print("Confusion matrix:")
-# print(cnf_matrix)
+
+
 with open("userlikes.pkl", "wb") as f:
     pickle.dump(clf, f, pickle.HIGHEST_PROTOCOL)
 
