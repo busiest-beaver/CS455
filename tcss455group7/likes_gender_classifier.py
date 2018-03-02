014f7f1b614de88ebd18371c7029a40e81c14551
wilderG <wemanuel22@hotmail.com>
2018-03-01 17:04:20 -0800

    Update likes_gender_classifier.py

--- tcss455group7/likes_gender_classifier.py
+++ tcss455group7/likes_gender_classifier.py
@@ -6,19 +6,20 @@ import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-
 class likes_gender_classifier:
 
     def __init__(self):
         '''empty constructor'''
 
     def __get_model(self):
-        file = open("/home/itadmin/src/CS455/likes/userlikes.pkl",'rb')
+         file = open("/data/userlikes.pkl",'rb')
+        # file = open("/home/itadmin/src/CS455/likes/userlikes.pkl",'rb')
         model = pickle.load(file)
         return model
 
     def __get_count_vectorizer(self):
-        file = open("/home/itadmin/src/CS455/likes/likeVectors.pkl",'rb')
+        # file = open("/home/itadmin/src/CS455/likes/likeVectors.pkl",'rb')
+        file = open("/data/likeVectors.pkl",'rb')
         cv = pickle.load(file)
         return cv
 
@@ -43,27 +44,21 @@ class likes_gender_classifier:
             print("Test directory to statuses not found.")
             exit()
 
-        # creating lists to store IDs and texts
-        usersIDs = []
-        likesIDs = []
-        df = pd.read_csv(input_dir+"relation.csv")
-        df['like_id'] = df['like_id'].astype(str)
-        list = []
-        for i in df['like_id']:
-            string = ' '.join(i)
-            list.append(string)
-        df['like_id'] = list #
-        df = df.reset_index()
 
-        vector = cv.transform(df['like_id'])
+        df = pd.read_csv(input_dir+"relation.csv").astype(str).drop_duplicates().groupby('userid')
+        userids = list(df.groups)
+        df = df.agg({'like_id':lambda x:' '.join(x)})#['like_id']
+        like_ids = df['like_id']
+
+
+        vector = cv.transform(list(like_ids))
 
         # using the count vector to predict gender
         prediction = model.predict(vector)
 
-        # adding the gender column predicted by our model to the dataframe
-        df['gender'] = prediction
 
         # using the ID and gender columns in our dataframe to create a dictionary
-        results = dict(zip(df['userid'], df['gender']))
-
+        results = dict(zip(userids, prediction))
+        #print(results)
         return results
+
