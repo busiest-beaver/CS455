6970d06c444c1ea34ef18bcc4d85ec32e0709d30
Abdullah <abdullahfmislam@gmail.com>
2018-03-05 03:50:05 -0800

    age ensemble (#35)
    * added age prediction using LIWC
    
    * added age classification ensemble

---
+++ tcss455group7/text_age_classifier.py
@@ -0,0 +1,134 @@
+import codecs
+import os
+import pickle
+from os.path import basename, exists, join, splitext
+
+import pandas as pd
+
+
+class liwc_age_classifier:
+
+    def __init__(self):
+        '''empty constructor'''
+
+    def __get_model(self):
+        svm_file = open("/home/itadmin/src/CS455/text/clf_svm.pkl",'rb')
+        rfc_file = open("/home/itadmin/src/CS455/text/clf_rfc.pkl",'rb')
+        rfc_liwc_file = open("/home/itadmin/src/CS455/text/clf_rfc_liwc.pkl",'rb')
+        
+        svm_model = pickle.load(svm_file)
+        rfc_model = pickle.load(rfc_file)
+        rfc_liwc_model = pickle.load(rfc_liwc_file)
+        
+        return rfc_model, rfc_liwc_model, svm_model
+
+    def __get_count_vectorizer(self):
+        file = open("/home/itadmin/src/CS455/text/newvec.pkl",'rb')
+        cv = pickle.load(file)
+        return cv
+
+    def test(self, **kwargs):
+        prediction = None
+        input_dir = kwargs['input_dir']
+
+        # loading the pickled NB model
+        rfc_model, rfc_model_liwc, svm_model = self.__get_model()
+
+        if (rfc_model == None or rfc_model_liwc == None or svm_model == None):
+            return {}
+
+        liwc_dir = ""
+        # checking if directory to the text files exist
+        if (os.path.isdir(input_dir+"/LIWC/")):
+            liwc_dir = input_dir+"/LIWC/"
+        elif (os.path.isdir(input_dir+"LIWC/")):
+            liwc_dir = input_dir+"/LIWC/"
+        else:
+            print("Test directory to LIWC not found.")
+            exit()
+
+        text_dir = ""
+        # checking if directory to the text files exist
+        if (os.path.isdir(input_dir+"/text/")):
+            text_dir = input_dir+"/text/"
+        elif (os.path.isdir(input_dir+"text/")):
+            text_dir = input_dir+"/text"
+        else:
+            print("Test directory to statuses not found.")
+            exit()
+
+        # Read in LIWC
+        df = pd.read_csv(liwc_dir)
+        #there is an inconsistency between 'userID' label in profile and LIWC, making sure the right column label is used
+        if 'userid' in df.columns:
+            df.rename(columns={'userid': 'userId'}, inplace=True)
+
+        # Read in text files
+        usersIDs = []
+        texts = []
+        df_text = pd.DataFrame()
+        
+        # populating the ID and text lists
+        for fileName in os.listdir(text_dir):
+            fileContent = codecs.open(text_dir+fileName, "r", encoding="utf-8", errors="ignore").read()
+            id = os.path.splitext(fileName)[0]
+            usersIDs.append(id)
+            texts.append(fileContent)
+
+        # adding the ID and text lists to dataframe
+        df_text['userId'] = usersIDs
+        df_text['text'] = texts
+
+        df.merge(df_text, 'outer', 'userId')
+        
+        #count vector for texts
+        cv = self.__get_count_vectorizer()
+        X_test = cv.transform(df['text'].values.astype('U'))
+
+        y_predicted_svm = svm_model.predict(X_test)
+
+        y_predicted_rfc = rfc_model.predict(X_test)
+
+        #column labels I want to use for LIWC
+        LIWC_features = ['Seg', 'WC', 'WPS', 'Sixltr', 'Dic', 'Numerals',
+       'funct', 'pronoun', 'ppron', 'i', 'we', 'you', 'shehe', 'they', 'ipron',
+       'article', 'verb', 'auxverb', 'past', 'present', 'future', 'adverb',
+       'preps', 'conj', 'negate', 'quant', 'number', 'swear', 'social',
+       'family', 'friend', 'humans', 'affect', 'posemo', 'negemo', 'anx',
+       'anger', 'sad', 'cogmech', 'insight', 'cause', 'discrep', 'tentat',
+       'certain', 'inhib', 'incl', 'excl', 'percept', 'see', 'hear', 'feel',
+       'bio', 'body', 'health', 'sexual', 'ingest', 'relativ', 'motion',
+       'space', 'time', 'work', 'achieve', 'leisure', 'home', 'money', 'relig',
+       'death', 'assent', 'nonfl', 'filler', 'Period', 'Comma', 'Colon',
+       'SemiC', 'QMark', 'Exclam', 'Dash', 'Quote', 'Apostro', 'Parenth',
+       'OtherP', 'AllPct']
+
+        #predicting using LIWC
+        X_test = df[LIWC_features]
+        y_predicted_rfc_liwc = rfc_model_liwc.predict(X_test)
+
+
+        y_predicted_final = ["xx-24"]*len(y_predicted_rfc)
+        
+        # ensemble
+        for i in range(len(y_predicted_final)):
+            if (y_predicted_rfc[i] == y_predicted_rfc_liwc[i]):
+                y_predicted_final[i] = y_predicted_rfc[i]
+            elif (y_predicted_rfc[i] == y_predicted_svm[i]):
+                y_predicted_final[i] = y_predicted_rfc[i]
+            elif (y_predicted_rfc_liwc[i] == y_predicted_svm[i]):
+                y_predicted_final[i] = y_predicted_rfc_liwc[i]
+
+        # adding the gender column predicted by our model to the dataframe
+        df['age'] = y_predicted_final
+
+        # using the ID and age columns in our dataframe to create a dictionary
+        results = dict(zip(df['userId'], df['age']))
+
+        return results
+
+
+
+
+
+
--- text/age_trainer.py
+++ text/age_trainer.py
@@ -1,9 +1,49 @@
 import pickle
+import random
 
+import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv("processedAge.csv")
+n = 8000
+all_Ids = np.arange(len(df))
+random.shuffle(all_Ids)
+test_Ids = all_Ids[0:n] 
+train_Ids = all_Ids[n:] 
+data_test = df.loc[test_Ids, :]
+data_train = df.loc[train_Ids, :]
+
+# Training a Naive Bayes model
+count_vect = CountVectorizer()
+X_train = count_vect.fit_transform(data_train['text'].values.astype('U'))
+y_train = data_train['ageLabel']
+newVec = CountVectorizer(vocabulary=count_vect.vocabulary_)
+X_test = newVec.transform(data_test['text'].values.astype('U'))
+y_test = data_test['ageLabel']
+
+clf_svc = SVC(probability=True, decision_function_shape='ovr')#criterion="gini", max_depth=82) 
+clf_svc.fit(X_train, y_train)
+y_predicted_svm = clf_svc.predict(X_test)
+# Reporting on classification performance
+print("Accuracy Text with SVM: %.2f" % accuracy_score(y_test, y_predicted_svm))
+scores = cross_val_score(clf_svc, X_train, y_train, cv=5)
+print("10-Fold Accuracy: %0.2f" % (scores.mean()))
+
+clf_rfc = RandomForestClassifier(criterion = 'gini')
+clf_rfc.fit(X_train, y_train)
+y_predicted_rfc = clf_rfc.predict(X_test)
+# Reporting on classification performance
+print("Accuracy Text with RFC: %.2f" % accuracy_score(y_test, y_predicted_rfc))
+scores = cross_val_score(clf_rfc, X_train, y_train, cv=5)
+print("10-Fold Accuracy: %0.2f" % (scores.mean()))
 
 LIWC_features = ['Seg', 'WC', 'WPS', 'Sixltr', 'Dic', 'Numerals',
        'funct', 'pronoun', 'ppron', 'i', 'we', 'you', 'shehe', 'they', 'ipron',
@@ -18,15 +58,36 @@ LIWC_features = ['Seg', 'WC', 'WPS', 'Sixltr', 'Dic', 'Numerals',
        'SemiC', 'QMark', 'Exclam', 'Dash', 'Quote', 'Apostro', 'Parenth',
        'OtherP', 'AllPct']
 
-df = pd.read_csv("processedAge.csv")
-X = df[LIWC_features]
-y = df["ageLabel"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1500)
-
-clf = RandomForestClassifier(criterion = 'gini')
-clf.fit(X_train, y_train)
-scores = cross_val_score(clf, X_train, y_train, cv=15)
+X_train = data_train[LIWC_features]
+X_test = data_test[LIWC_features]
+clf_rfc_liwc = RandomForestClassifier(criterion = 'gini')
+clf_rfc_liwc.fit(X_train, y_train)
+y_predicted_rfc_liwc = clf_rfc_liwc.predict(X_test)
+scores = cross_val_score(clf_rfc_liwc, X_train, y_train, cv=15)
 print("10-Fold Accuracy: %0.2f" % (scores.mean()))
 
-with open("liwc_gender_classifier.pkl", "wb") as f:
-    pickle.dump(clf, f, pickle.HIGHEST_PROTOCOL)
+
+y_predicted_final = [None]*8000
+print(len(y_predicted_rfc), len(y_predicted_rfc_liwc), len(y_predicted_svm))
+# ensemble
+for i in range(len(y_predicted_final)):
+    if (y_predicted_rfc[i] == y_predicted_rfc_liwc[i]):
+        y_predicted_final[i] = y_predicted_rfc[i]
+    elif (y_predicted_rfc[i] == y_predicted_svm[i]):
+        y_predicted_final[i] = y_predicted_rfc[i]
+    elif (y_predicted_rfc_liwc[i] == y_predicted_svm[i]):
+        y_predicted_final[i] = y_predicted_rfc_liwc[i]
+    else:
+        print("crap")
+        y_predicted_final[i] = "xx-24"
+
+print("Accuracy Text with All 3: %.2f" % accuracy_score(y_test, y_predicted_final))
+
+with open("clf_rfc.pkl", "wb") as f:
+    pickle.dump(clf_rfc, f, pickle.HIGHEST_PROTOCOL)
+with open("clf_rfc_liwc.pkl", "wb") as f:
+    pickle.dump(clf_rfc_liwc, f, pickle.HIGHEST_PROTOCOL)
+with open("clf_svm.pkl", "wb") as f:
+    pickle.dump(clf_svc, f, pickle.HIGHEST_PROTOCOL)
+with open("newvec.pkl", "wb") as f:
+    pickle.dump(newVec, f, pickle.HIGHEST_PROTOCOL)
